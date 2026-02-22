19f2db946acf39cfcd18720049539e9d07a61f21
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-22 18:09:12 +0200

    chore: add pyspark dependency

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+from pyspark import __version__ as pyspark_version
+
+
 def main():
-    print("Hello from yelp-spark-project!")
+    print(
+        f"Hello from yelp-spark-project!\n"
+        f"Using PySpark version {pyspark_version}"
+    )
 
 
 if __name__ == "__main__":
